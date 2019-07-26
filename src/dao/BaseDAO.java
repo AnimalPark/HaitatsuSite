7e5e901d2784599651cb094d9b3425d26cc00a11
AnimalPark <dhpark3610@gmail.com>
2019-07-26 18:11:59 +0900

    BaseDAO 추가

--- src/dao/BaseDAO.java
+++ src/dao/BaseDAO.java
@@ -1,5 +1,61 @@
 package dao;
 
-public class BaseDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+
+public class BaseDAO {
+	String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	String USER = "madang";
+	String PASSWORD = "madang";
+
+	public Connection getConnection() {
+
+		Connection connection = null;
+
+		try {
+			Class.forName(DRIVER);
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return connection;
+	}
+
+	public void closeDBObjects(ResultSet resultSet, Statement statement, Connection connection) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (statement != null) {
+			try {
+				statement.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
+
