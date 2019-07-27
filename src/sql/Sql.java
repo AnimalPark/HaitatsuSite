1c7fc346b249b59e0895f9383b24e6358523a603
AnimalPark <dhpark3610@gmail.com>
2019-07-27 17:40:41 +0900

    dd

--- src/sql/Sql.java
+++ src/sql/Sql.java
@@ -5,14 +5,14 @@ public class Sql {
 			+ "(SEQ_QA_NO.NEXTVAL,?,?,SYSDATE,0,?,SEQ_QA_NO.CURRVAL,0)";
 	public static final String YBBS_INSERT_REPLY_SQL = "INSERT INTO YBBS_QA VALUES "
 			+ "(SEQ_QA_NO.NEXTVAL,?,?,SYSDATE,0,?,?,1)";
-	public static final String YBBS_SELECTALL_SQL = "SELECT NO,SUBJECT,ID,WDATE,GRP,LVL,VISITED "
-			+ "FROM YBBS "
-			+ "ORDER BY GRP DESC,LVL ASC,WDATE DESC";
-	public static final String YBBS_SELECTBYNO_SQL = "SELECT NO,ID,SUBJECT,CONTENT FROM YBBS WHERE NO = ?";
-	public static final String YBBS_UPDATE_SQL = "UPDATE YBBS SET SUBJECT = ? , CONTENT = ? WHERE NO = ?";
-	public static final String YBBS_DELETE_SQL = "DELETE FROM YBBS WHERE NO = ?";
-	public static final String YBBS_UPDATE_VISITED = "UPDATE ybbs SET visited = visited+1 where no = ?";
-	public static final String YBBS_PAGE_SQL = "SELECT * from (SELECT ROWNUM RN,ybbss.* FROM (SELECT * FROM YBBS ORDER BY GRP DESC,LVL ASC,WDATE DESC) ybbss) WHERE RN BETWEEN ? AND ?";
+    public static final String YBBS_SELECTALL_SQL = "SELECT QANUMBER,QASUBJECT,QADATE,QAVISITED,USERID,QAGROUP,QALEVEL "
+            + "FROM YBBS_QA"
+            + "ORDER BY QAGROUP DESC,QALEVEL ASC,QADATE DESC";
+    public static final String YBBS_SELECTBYNO_SQL = "SELECT QANUMBER,USERID,QASUBJECT,QACOMMENT FROM YBBS_QA WHERE QANUMBER = ?";
+    public static final String YBBS_UPDATE_SQL = "UPDATE YBBS_QA SET QASUBJECT = ? , QACOMMENT = ? WHERE QANUMBER = ?";
+    public static final String YBBS_DELETE_SQL = "DELETE FROM YBBS_QA WHERE QANUMBER = ?";
+    public static final String YBBS_UPDATE_VISITED = "UPDATE YBBS_QA SET QAVISITED = QAVISITED+1 WHERE QANUMBER = ?";
+    public static final String YBBS_PAGE_SQL = "SELECT * from (SELECT ROWNUM RN,ybbss.* FROM (SELECT * FROM YBBS_QA ORDER BY QAGROUP DESC,QALEVEL ASC,QADATE DESC) ybbss) WHERE RN BETWEEN ? AND ?";
     public static final String YBBS_SELECT_ALL_COUNT ="SELECT COUNT(*) AS cnt FROM YBBS";
 	
 	//==========================¹Ú¼ºÇõ Sql=======================================
