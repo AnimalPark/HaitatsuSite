7e7a5a83d43270c7509fbfee42bfe562bb77a9bf
AnimalPark <dhpark3610@gmail.com>
2019-08-01 16:51:08 +0900

    ㅇㅇ

--- src/sql/Sql.java
+++ src/sql/Sql.java
@@ -38,7 +38,6 @@ public class Sql {
 							"WHERE CITYNUM = (SELECT CITYNUM " + 
 											"FROM CITY " + 
 											"WHERE CITYNAME = ? ) AND TOWNNAME = ? )";
-	public static final String RESTAURANT_SELECT_BY_RNUM_SQL = "SELECT * FROM RESTAURANT WHERE RNUM = ?";
 	
 	//==========================È²È£¿µ Sql¡é=======================================
 	public static final String RESTAURANT_SELECT_ALL_SQL = "SELECT * FROM restaurant";
