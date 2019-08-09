bbf184402560d0c9a27582cc6dc5ee3a316836b0
ryunni <ryun5757@gmail.com>
2019-08-09 14:16:16 +0900

    no message

---
+++ src/filter/AdminCheckFilter.java
@@ -0,0 +1,39 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "AdminCheckFilter", urlPatterns = {"*.ad"})
+public class AdminCheckFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpSession session = request.getSession(false);
+
+		boolean login = false;
+
+		if (session != null) {
+			if (session.getAttribute("users") != null) {
+				login = true;
+			}
+		}
+
+		if (login) {
+			chain.doFilter(req, res);
+		} else {
+			((HttpServletResponse) res).sendRedirect("/HaitatsuSite/user_join");
+		}
+	}
+}
